872f94ad3de5d218cd2416aacb443f7aee6d083f
Liz Peacock <liz.peacock11@gmail.com>
2016-09-16 16:34:37 -0500

    adding insert lesson

---
+++ albums_seeder.sql
@@ -0,0 +1,9 @@
+USE codeup_test_db;
+INSERT INTO albums (artist, name, release_date, sales, genre) VALUES ('Michael Jackson', 'Thriller', 1982, 46, 'Pop, Rock, R&B');
+INSERT INTO albums (artist, name, release_date, sales, genre) VALUES ('AC/DC', 'Back in Black', 1980, 26.1, 'Hard Rock');
+INSERT INTO albums (artist, name, release_date, sales, genre) VALUES ('Pink Floyd', 'The Dark Side of the Moon', 1973, 24.2, 'Progressive Rock');
+INSERT INTO albums (artist, name, release_date, sales, genre) VALUES ('Whitney Houston/Various Artists', 'The Bodyguard', 1992, 27.4, 'Soundtrack, R&B, Soul, Pop');
+INSERT INTO albums (artist, name, release_date, sales, genre) VALUES ('Meat Loaf', 'Bat Out of Hell', 1977, 20.6, 'Hard Rock, Progressive Rock');
+INSERT INTO albums (artist, name, release_date, sales, genre) VALUES ('Eagles', 'Their Greatest Hits (1971-1975)', 1976, 32.2, 'Rock, Soft Rock, Folk Rock');
+INSERT INTO albums (artist, name, release_date, sales, genre) VALUES ('Bee Gees/Various Artists', 'Saturday Night Fever', 1977, 20.6, 'Disco');
+INSERT INTO albums (artist, name, release_date, sales, genre) VALUES ('Fleetwood Mac', 'Rumours', 1977, 27.9, 'Soft Rock');
