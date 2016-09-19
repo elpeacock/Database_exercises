020cd538db0a230b3112a89aeb4d6461bcf02ee2
Liz Peacock <liz.peacock11@gmail.com>
2016-09-19 16:47:32 -0500

    adding step 3 query - hired in the 1990's

--- where_exercises.sql
+++ where_exercises.sql
@@ -1,4 +1,5 @@
 SELECT * FROM employees WHERE first_name IN ('Irena', 'Vidya', 'Maya');
 SELECT * FROM employees WHERE last_name LIKE 'E%';
+SELECT * FROM employees WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31';
 
 
