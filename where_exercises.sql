2989dca5c2672613e3bc32196b3be4475f8f876a
Liz Peacock <liz.peacock11@gmail.com>
2016-09-19 16:52:58 -0500

    adding last step query for last name with q

--- where_exercises.sql
+++ where_exercises.sql
@@ -2,5 +2,6 @@ SELECT * FROM employees WHERE first_name IN ('Irena', 'Vidya', 'Maya');
 SELECT * FROM employees WHERE last_name LIKE 'E%';
 SELECT * FROM employees WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31';
 SELECT * FROM employees WHERE birth_date LIKE '19%-12-25';
+SELECT * FROM employees WHERE last_name LIKE '%Q%';
 
 
