7ea2bb471ad58bed7122f0f590e16e31dec1efaf
Liz Peacock <liz.peacock11@gmail.com>
2016-09-20 10:01:43 -0500

    addtl queries added or refactored to combine + and/or/not clauses

--- where_exercises.sql
+++ where_exercises.sql
@@ -1,7 +1,7 @@
-SELECT * FROM employees WHERE first_name IN ('Irena', 'Vidya', 'Maya');
-SELECT * FROM employees WHERE last_name LIKE 'E%';
-SELECT * FROM employees WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31';
-SELECT * FROM employees WHERE birth_date LIKE '19%-12-25';
-SELECT * FROM employees WHERE last_name LIKE '%Q%';
+SELECT * FROM employees WHERE (first_name = 'Irena' OR first_name = 'Vidya' OR first_name = 'Maya') AND gender = 'M';
+SELECT * FROM employees WHERE last_name LIKE 'e%' OR last_name LIKE '%e';
+SELECT * FROM employees WHERE last_name LIKE 'e%e';
+SELECT * FROM employees WHERE hire_date LIKE '199%' AND birth_date LIKE '%-12-25';
+SELECT * FROM employees WHERE last_name LIKE '%Q%' AND last_name NOT LIKE '%QU%';
 
 
