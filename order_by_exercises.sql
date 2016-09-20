fafec2b9b0d2f77fe735c70fc760e87c253caaa4
Liz Peacock <liz.peacock11@gmail.com>
2016-09-20 10:36:03 -0500

    adding step 3, refactored to order by last name then first name

--- order_by_exercises.sql
+++ order_by_exercises.sql
@@ -1,6 +1,6 @@
 SELECT * FROM employees 
 WHERE (first_name = 'Irena' OR first_name = 'Vidya' OR first_name = 'Maya') 
-ORDER BY first_name ASC, last_name ASC;
+ORDER BY last_name ASC, first_name ASC;
 SELECT * FROM employees WHERE last_name LIKE 'e%' OR last_name LIKE '%e';
 SELECT * FROM employees WHERE last_name LIKE 'e%e';
 SELECT * FROM employees WHERE hire_date LIKE '199%' AND birth_date LIKE '%-12-25';
