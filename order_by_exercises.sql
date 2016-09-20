b00dda7f0aeab53b17a34783d82be287cba10c0e
Liz Peacock <liz.peacock11@gmail.com>
2016-09-20 10:48:43 -0500

    ordered by born on xmas to show oldest employee who was hired last

--- order_by_exercises.sql
+++ order_by_exercises.sql
@@ -9,7 +9,11 @@ ORDER BY emp_no DESC;
 SELECT * FROM employees 
 WHERE last_name LIKE 'e%e'
 ORDER BY emp_no DESC;
-SELECT * FROM employees WHERE hire_date LIKE '199%' AND birth_date LIKE '%-12-25';
+
+SELECT * FROM employees 
+WHERE hire_date LIKE '199%' AND birth_date LIKE '%-12-25'
+ORDER BY birth_date ASC, hire_date DESC;
+
 SELECT * FROM employees WHERE last_name LIKE '%Q%' AND last_name NOT LIKE '%QU%';
 
 
