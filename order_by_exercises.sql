13fa1c2cf8e29e05d73882657e11cc1bbc5213ed
Liz Peacock <liz.peacock11@gmail.com>
2016-09-20 10:45:21 -0500

    updated e name queries to sort in descending order

--- order_by_exercises.sql
+++ order_by_exercises.sql
@@ -4,11 +4,11 @@ ORDER BY last_name ASC, first_name ASC;
 
 SELECT * FROM employees 
 WHERE last_name LIKE 'e%' OR last_name LIKE '%e'
-ORDER BY emp_no;
+ORDER BY emp_no DESC;
 
 SELECT * FROM employees 
 WHERE last_name LIKE 'e%e'
-ORDER BY emp_no;
+ORDER BY emp_no DESC;
 SELECT * FROM employees WHERE hire_date LIKE '199%' AND birth_date LIKE '%-12-25';
 SELECT * FROM employees WHERE last_name LIKE '%Q%' AND last_name NOT LIKE '%QU%';
 
