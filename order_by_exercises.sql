c48be0af9e00e7994c3d52932e3db1aeb993340e
Liz Peacock <liz.peacock11@gmail.com>
2016-09-20 10:43:27 -0500

    updated e in the last name queries to be sorted by emp_no ascending

--- order_by_exercises.sql
+++ order_by_exercises.sql
@@ -1,8 +1,14 @@
 SELECT * FROM employees 
 WHERE (first_name = 'Irena' OR first_name = 'Vidya' OR first_name = 'Maya') 
 ORDER BY last_name ASC, first_name ASC;
-SELECT * FROM employees WHERE last_name LIKE 'e%' OR last_name LIKE '%e';
-SELECT * FROM employees WHERE last_name LIKE 'e%e';
+
+SELECT * FROM employees 
+WHERE last_name LIKE 'e%' OR last_name LIKE '%e'
+ORDER BY emp_no;
+
+SELECT * FROM employees 
+WHERE last_name LIKE 'e%e'
+ORDER BY emp_no;
 SELECT * FROM employees WHERE hire_date LIKE '199%' AND birth_date LIKE '%-12-25';
 SELECT * FROM employees WHERE last_name LIKE '%Q%' AND last_name NOT LIKE '%QU%';
 
