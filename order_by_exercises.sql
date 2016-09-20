53657abe9619d79d15fc4218203a70a0040a7f3f
Liz Peacock <liz.peacock11@gmail.com>
2016-09-20 10:31:27 -0500

    added part one, order irina, maya, vidya by first name

---
+++ order_by_exercises.sql
@@ -0,0 +1,9 @@
+SELECT * FROM employees 
+WHERE (first_name = 'Irena' OR first_name = 'Vidya' OR first_name = 'Maya') 
+ORDER BY first_name;
+SELECT * FROM employees WHERE last_name LIKE 'e%' OR last_name LIKE '%e';
+SELECT * FROM employees WHERE last_name LIKE 'e%e';
+SELECT * FROM employees WHERE hire_date LIKE '199%' AND birth_date LIKE '%-12-25';
+SELECT * FROM employees WHERE last_name LIKE '%Q%' AND last_name NOT LIKE '%QU%';
+
+
